26f2bfcc484b632b59968df4bb9836936824fbe7
Antti Ketola <antti@anttiketola.com>
2022-03-24 18:02:27 +0200

    Task 9.1 done

---
+++ Assignments_B/bmiCalculator.ts
@@ -0,0 +1,17 @@
+
+const calculateBmi = (height: number, weight: number): string => {
+    const BMI = (weight / ((height/100)**2));
+    
+    if (BMI < 16) return "Underweight (Severe thinness)";
+    if (BMI >= 16 && BMI < 17) return "Underweight (Moderate thinness)";
+    if (BMI >= 17 && BMI < 18.5) return "Underweight (Mild thinness)";
+    if (BMI >= 18.5 && BMI < 25) return "Normal (healthy weight)";
+    if (BMI >= 25 && BMI < 30) return "Overweight (Pre-obese)";
+    if (BMI >= 30 && BMI < 35) return "Obese (Class I)";
+    if (BMI >= 35 && BMI < 40) return "Obese (Class II)";
+    if (BMI >= 40) return "Obese (Class III)";
+    
+}
+
+
+console.log(calculateBmi(182, 74))
